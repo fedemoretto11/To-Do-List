5547e35afc23505ff01588febe79f17ac06267df
fedemoretto11 <fedemoretto94@gmail.com>
2023-01-09 18:23:04 -0300

    Boton de importancia agregado, sin JS todavia

--- script.js
+++ script.js
@@ -5,11 +5,23 @@ const ul = document.querySelector("ul");
 
 let tareasPorHacer = [];
 
+
+
+// Ambos Event Listener cumplen la misma funcion (uno clickea y el otro enter)
 icono.addEventListener("click", (e) => {
     const li = document.createElement("li");
     li.appendChild(document.createTextNode(tareas.value));
     ul.appendChild(li);
     tareasPorHacer.push(tareas.value);
+    tareas.value = " ";   
+})
+
+document.querySelector("input").addEventListener("keypress", (e) => {
+    if(e.key === 'Enter') {
+    const li = document.createElement("li");
+    li.appendChild(document.createTextNode(tareas.value));
+    ul.appendChild(li);
+    tareasPorHacer.push(tareas.value);
     tareas.value = " ";
-    
+} 
 })
