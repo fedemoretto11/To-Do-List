78cb2a587e13cb1ac5b06ea491d20c728a11b6ea
fedemoretto11 <fedemoretto94@gmail.com>
2023-01-19 12:22:55 -0300

    HTML Y JS, falta darle estilo

--- script.js
+++ script.js
@@ -1,27 +1,52 @@
-const tareas = document.querySelector(".toDo");
-const porHacer = document.querySelector(".tareas");
-const icono = document.querySelector(".icono");
-const ul = document.querySelector("ul");
+const dateNumber = document.getElementById('dateNumber');
+const dateMonth = document.getElementById('dateMonth');
+const dateYear = document.getElementById('dateYear');
+const dateText = document.getElementById('dateText');
 
-let tareasPorHacer = [];
+const taskContainer = document.getElementById('taskContainer');
+
+const setDate = () => {
+    const date = new Date();
+
+    dateNumber.textContent = date.toLocaleString('es', { day: 'numeric' });
+    dateMonth.textContent = date.toLocaleString('es', { month: 'short' });
+    dateYear.textContent = date.toLocaleString('es', { year: 'numeric' });
+    dateText.textContent = date.toLocaleString('es', { weekday: 'long' });   
+    
+
+};
+
+const addNewTask = event => {
+    event.preventDefault();
+    const { value } = event.target.taskText;
+    if(!value) return;
+    const task = document.createElement('div');
+    task.classList.add('task', 'roundBorder');
+    task.addEventListener('click', changeTaskState)
+    task.textContent = value;
+    taskContainer.prepend(task);
+    event.target.reset();
+};
+
+const changeTaskState = event => {
+    event.target.classList.toggle('done')
+};
+
+const order = () => {
+    const done = [];
+    const toDo = [];
+    taskContainer.childNodes.forEach(el => {
+        el.classList.contains('done') ? done.push(el) : toDo.push(el);
+    })
+    return [...toDo, ...done]
+}
+
+const renderOrderedTasks = () => {
+    order().forEach(el => { taskContainer.appendChild(el)})
+}
 
 
 
-// Ambos Event Listener cumplen la misma funcion (uno clickea y el otro enter)
-icono.addEventListener("click", (e) => {
-    const li = document.createElement("li");
-    li.appendChild(document.createTextNode(tareas.value));
-    ul.appendChild(li);
-    tareasPorHacer.push(tareas.value);
-    tareas.value = " ";   
-})
 
-document.querySelector("input").addEventListener("keypress", (e) => {
-    if(e.key === 'Enter') {
-    const li = document.createElement("li");
-    li.appendChild(document.createTextNode(tareas.value));
-    ul.appendChild(li);
-    tareasPorHacer.push(tareas.value);
-    tareas.value = " ";
-} 
-})
+
+setDate();
